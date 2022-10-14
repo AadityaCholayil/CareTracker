4409bbf28c4ef5105492a711a0c3e03621e04ffb
AadityaCholayil <cholayilaaditya@gmail.com>
2022-10-14 13:05:30 +0530

    App Created

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:bloc_concurrency/bloc_concurrency.dart';
+import 'package:care_tracker/bloc/app_bloc_observer.dart';
+import 'package:care_tracker/firebase_options.dart';
+import 'package:care_tracker/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  Bloc.observer = MyAppBlocObserver();
+  Bloc.transformer = sequential<dynamic>();
+  runApp(const FlutterFireInit());
+}
+
+class FlutterFireInit extends StatefulWidget {
+  const FlutterFireInit({Key? key}) : super(key: key);
+
+  @override
+  _FlutterFireInitState createState() => _FlutterFireInitState();
+}
+
+class _FlutterFireInitState extends State<FlutterFireInit> {
+  Future<FirebaseApp> _initialization() async {
+    return await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization(),
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          // return const MaterialApp(home: SomethingWentWrong());
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        // return const LoadingPage();
+        return Container();
+      },
+    );
+  }
+}
